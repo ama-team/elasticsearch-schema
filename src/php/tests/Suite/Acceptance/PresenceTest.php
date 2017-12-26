3bc562a8d44e2181dc148c334149d5ecc13d10f2
Etki <etki@etki.me>
2017-12-26 19:30:58 +0300

    Added basic PHP testing

---
+++ src/php/tests/Suite/Acceptance/PresenceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AmaTeam\ElasticSearch\Schema\Definition\Test\tests\Suite\Acceptance;
+
+use AmaTeam\ElasticSearch\Schema\Definition\Reader;
+use Codeception\Test\Unit;
+use PHPUnit\Framework\Assert;
+
+class PresenceTest extends Unit
+{
+    /**
+     * @test
+     */
+    public function elasticSearchSchemaExists()
+    {
+        Assert::assertNotNull(Reader::readSchema('elasticsearch'));
+    }
+
+    /**
+     * @test
+     */
+    public function missingSchemaIsMissing()
+    {
+        Assert::assertNull(Reader::readSchema('missing'));
+    }
+}
