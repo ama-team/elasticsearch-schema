c6c57b96c111dc31527f5d7fddcc1306ede79588
Etki <etki@etki.me>
2017-12-26 18:36:16 +0300

    Added basic PHP support

---
+++ src/php/Locator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AmaTeam\ElasticSearch\Schema\Definition;
+
+class Locator
+{
+    /**
+     * Returns path to schema for specified product (or null if no
+     * such product is registered).
+     *
+     * @param string $product
+     *
+     * @return string|null
+     */
+    public static function locateSchema($product)
+    {
+        $segments = [ProjectStructure::getSchemaRoot(), $product . '.yml'];
+        $path = implode(DIRECTORY_SEPARATOR, $segments);
+        return file_exists($path) ? $path : null;
+    }
+}
